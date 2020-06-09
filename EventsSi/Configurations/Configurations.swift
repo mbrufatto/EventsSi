064114ba6f16335cee4f5918c85f543aedead678
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-06-09 09:41:59 -0300

    - Criada a estrutura do projeto com alguns parâmetros e bibliotecas já configurados e bibliotecas;

---
+++ EventsSi/Configurations/Configurations.swift
@@ -0,0 +1,14 @@
+//
+//  Configurations.swift
+//  EventsSi
+//
+//  Created by Marcio Habigzang Brufatto on 09/06/20.
+//  Copyright © 2020 Mantra Tech. All rights reserved.
+//
+
+import Foundation
+
+struct ApiConfig {
+    static let baseUrl = "http://5b840ba5db24a100142dcd8c.mockapi.io/api/events"
+    static let checkinUrl = "http://5b840ba5db24a100142dcd8c.mockapi.io/api/checkin"
+}
